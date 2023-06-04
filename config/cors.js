fe126b3a33d1430fc481228921ff9f9f2dc06767
Chavi Surujbhali <chavi@zunko.app>
2023-06-04 18:17:00 +0400

    final cors

---
+++ config/cors.js
@@ -0,0 +1,7 @@
+module.exports.cors = {
+    allRoutes: true,
+    allowOrigins: ['https://purple-stone-0a8985310-3.centralus.3.azurestaticapps.net'],
+    allowCredentials: false,
+    allowRequestMethods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+    allowRequestHeaders: 'content-type'
+  };
